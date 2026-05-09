02317d19edbbfa89b539a0e333d9b79418469196
GliciniaD <190259001+GliciniaD@users.noreply.github.com>
2026-05-09 08:25:51 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "vnet_peering" {
   type        = map(object({
     rg_name = string
-    storage_name = string
+    peering_name = string
+    vnet_name = string
+    remote_vnet_id = string
 }))
 }
