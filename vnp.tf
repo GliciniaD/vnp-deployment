75da6b959f061b82eda43611a2c0285abf581e6e
GliciniaD <190259001+GliciniaD@users.noreply.github.com>
2026-05-09 08:13:34 +0100

    Update vnp.tf

--- vnp.tf
+++ vnp.tf
@@ -5,10 +5,10 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "vnetpeering" {
-  for_each                 = var.vnet_peering
-  name              = var.peering_name
-  resource_group_name      = azurerm_resource_group.rg[each.key].name
-  location                 = azurerm_resource_group.rg[each.key].location
+  for_each                  = var.vnet_peering
+  name                      = var.peering_name
+  resource_group_name       = azurerm_resource_group.rg[each.key].name
+  location                  = azurerm_resource_group.rg[each.key].location
   virtual_network_name      = var.vnet_name
-   remote_virtual_network_id =var.remote_vnet_id 
+  remote_virtual_network_id = var.remote_vnet_id 
 }
